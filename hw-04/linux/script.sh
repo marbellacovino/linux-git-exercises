a680c89a3d6ae888b23e88be47d31a3e609082ff
Marbella <marbella30392@gmail.com>
2021-02-02 11:05:26 +0100

    dayweek fixed

--- hw-04/linux/script.sh
+++ hw-04/linux/script.sh
@@ -9,7 +9,7 @@ else
   ###  Control will jump here if $DIR does NOT exists ###
   mkdir -p "$DIR"
   cp ~/nginx_requests_total.txt "$DIR"/nginx_requests_total_$(date +"%Y%m%d").txt 
-  if [ "$TODAY" -eq 1 ]; then
+  if [ "$TODAY" -eq 6 ]; then
    cd backup/marbellacovino/$(date +"%Y")/$(date +"%m")
    find . -name '*.txt' -mtime -6 | tar -zcvf nginx_logs_$(date +"%Y%m%d").tar.gz -T -
   fi
